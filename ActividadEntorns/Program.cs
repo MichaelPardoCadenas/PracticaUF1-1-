f4b70fc60a89fa794845b45465c2ab6337f457d4
MichaelPardoCadenas <michael.pardo@fjaverianas.estudiant.com>
2023-11-10 11:32:45 +0100

    Código Subido

---
+++ ActividadEntorns/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number;
+        Console.Write("Introduce un número del 1 al 3: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        switch (number)
+        {
+            case 1:
+                Console.WriteLine("Red");
+                break;
+            case 2:
+                Console.WriteLine("Yellow");
+                break;
+            case 3:
+                Console.WriteLine("Green");
+                break;
+            default:
+                Console.WriteLine("Has introducido un valor invalido");
+                break;
+        }
+    }
+}
